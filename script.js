8ed68b20a3716cf749971b871e5231eaa3e340ee
Yasya Mashnenko <yasyamashnenko@MacBook-Air-Daniil.local>
2020-10-29 09:27:11 +0100

    table

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(document.body.firstElementChild);
+console.log(document.body.firstElementChild.nextElementSibling);
+console.log(document.body.firstElementChild.nextElementSibling.lastElementChild)
+
+console.log(document.body.children[0]);
