00afdc7f3ce9110e3bfe7f486e2089fc36e52fb3
ya-mashnenko <72609097+ya-mashnenko@users.noreply.github.com>
2020-11-09 15:37:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
-console.log(document.body.firstElementChild);
-console.log(document.body.firstElementChild.nextElementSibling);
-console.log(document.body.firstElementChild.nextElementSibling.lastElementChild)
+const addButton = document.querySelector('.addButton');
+const input = document.querySelector('.input');
+const container = document.querySelector('.container');
 
-console.log(document.body.children[0]);
+class Item {
+  constructor(itemName) {
+    this.createDiv(itemName)
+  }
+
+  createDiv(itemName) {
+    let input = document.createElement('input');
+    input.value = itemName;
+    input.disabled = true;
+    input.classList.add('itemInput');
+    input.type = 'text';
+
+    let itemBox = document.createElement('div');
+    itemBox.classList.add('item');
+
+    let editButton = document.createElement('button');
+    editButton.innerHTML = 'edit';
+    editButton.classList.add('editButton');
+
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML = 'remove';
+    removeButton.classList.add('removeButton');
+
+    container.appendChild(itemBox);
+
+    itemBox.appendChild(input);
+    itemBox.appendChild(editButton);
+    itemBox.appendChild(removeButton);
+
+    editButton.addEventListener('click', () => this.edit(input));
+
+    removeButton.addEventListener('click', () => this.remove(itemBox));
+  }
+
+  edit(input) {
+    input.disabled = !input.disabled;
+    input.select();
+  };
+
+  remove(item) {
+    container.removeChild(item);
+  }
+};
+
+function addItem() {
+  if (input.value !== '') {
+    new Item(input.value);
+    input.value = '';
+  }
+}
+
+addButton.addEventListener('click', addItem);
+
+window.addEventListener('keydown', (e) => {
+  if (e.which === 13) {
+    addItem();
+  }
+})
